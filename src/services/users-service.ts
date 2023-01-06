5bbd81c61d4e8ac2ff2c3a4bf738aa234154bee8
tokensm1x <tokensmix@gmail.com>
2023-01-06 13:54:14 +0300

    feat: errors handling

--- src/services/users-service.ts
+++ src/services/users-service.ts
@@ -3,7 +3,7 @@ import { usersPath } from "../app/constants";
 import { v4 as uuid_v4, validate } from "uuid";
 import { User } from "../entity/user";
 import { NotFoundError, ValidationError } from "../app/errors";
-import { INVALID_DATA_ERROR, INVALID_ID_ERROR, USER_NOT_FOUND_ERROR } from "../app/constants";
+import { INVALID_ID_ERROR, USER_NOT_FOUND_ERROR, validationErrors } from "../app/constants";
 import { fork } from "child_process";
 import cluster from "cluster";
 
@@ -82,15 +82,39 @@ export class UserService {
 
     public validateData(body: IUser): void {
         if (body.id) delete body.id;
+        let error = "";
+        const {
+            USERNAME_NOT_EXIST_ERROR,
+            AGE_NOT_EXIST_ERROR,
+            HOBBIES_NOT_EXIST_ERROR,
+            USERNAME_IS_INVALID_ERROR,
+            AGE_IS_INVALID_ERROR,
+            HOBBIES_IS_INVALID_ERROR,
+        } = validationErrors;
         const username: string = body.username;
         const age: number = body.age;
         const hobbies: string[] = body.hobbies;
         const usernameIsString: boolean = typeof username === "string";
         const ageIsNumber: boolean = typeof age === "number";
         const hobbiesIsArray: boolean = Array.isArray(hobbies);
-        const isValidHobbies: boolean = hobbies?.every((el: any) => typeof el === "string");
-        if (!username || !usernameIsString || !age || !ageIsNumber || !hobbiesIsArray || !hobbies || !isValidHobbies) {
-            throw new ValidationError(INVALID_DATA_ERROR);
+        const isValidHobbies: boolean = hobbiesIsArray ? hobbies?.every((el: any) => typeof el === "string") : false;
+        if (!username) {
+            error = USERNAME_NOT_EXIST_ERROR;
+        } else if (!usernameIsString) {
+            error = USERNAME_IS_INVALID_ERROR;
+        } else if (!age) {
+            error = AGE_NOT_EXIST_ERROR;
+        } else if (!ageIsNumber) {
+            error = AGE_IS_INVALID_ERROR;
+        } else if (!hobbiesIsArray) {
+            error = HOBBIES_IS_INVALID_ERROR;
+        } else if (!hobbies) {
+            error = HOBBIES_NOT_EXIST_ERROR;
+        } else if (!isValidHobbies) {
+            error = HOBBIES_IS_INVALID_ERROR;
+        }
+        if (error) {
+            throw new ValidationError(error);
         }
     }
 }
