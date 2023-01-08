88df2a7004e21d33d1fe8b1d4b0426433ea7c95a
tokensm1x <tokensmix@gmail.com>
2023-01-08 16:59:39 +0300

    fix: tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,24 +1,22 @@
 import { workerServer } from "../src/app/app";
 import supertest from "supertest";
 
-const apiUrl = "api/users";
-
+const apiUrl = "/api/users";
 describe("Scenario 1: api execution", () => {
     it("GET EMPTY ARRAY USERS", async () => {
         const expected = [];
         const response = await supertest(workerServer).get(apiUrl);
-        console.log(response);
         expect(response.statusCode).toBe(200);
         expect(response.body).toEqual(expected);
     });
-    it("test 2", () => {});
-    it("test 3", () => {});
-    it("test 4", () => {});
-    it("test 5", () => {});
-    it("test 6", () => {});
+    it("CREATE USER", () => {});
+    it("UPDATE USER", () => {});
+    it("GET USER BY ID", () => {});
+    it("DELETE USER", () => {});
+    it("GET DELETED USER", () => {});
 });
 
-describe("Scenario 2", () => {
+describe("Scenario 2: test validation", () => {
     it("test 1", () => {});
     it("test 2", () => {});
     it("test 3", () => {});
@@ -27,7 +25,7 @@ describe("Scenario 2", () => {
     it("test 6", () => {});
 });
 
-describe("Scenario 3", () => {
+describe("Scenario 3: test errors", () => {
     it("test 1", () => {});
     it("test 2", () => {});
     it("test 3", () => {});
